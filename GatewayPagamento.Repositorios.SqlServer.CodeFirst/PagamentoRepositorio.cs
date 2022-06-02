abb8aa5310ee6b18b3f2a0e4431c1c872c3ee04a
Vitor Neil Avelino <avelino.vitor@gmail.com>
2022-06-01 22:59:29 -0300

    GatewayPagamento - domínio e repositório

---
+++ GatewayPagamento.Repositorios.SqlServer.CodeFirst/PagamentoRepositorio.cs
@@ -0,0 +1,28 @@
+﻿using GatewayPagamento.Dominio.Entidades;
+using GatewayPagamento.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GatewayPagamento.Repositorios.SqlServer.CodeFirst
+{
+    public class PagamentoRepositorio : IPagamentoRepositorio
+    {
+        public void Inserir(Pagamento pagamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Pagamento> Selecionar(string numeroCartao)
+        {
+            using (var contexto = new GatewayPagamentoDbContext())
+            {
+                return contexto.Pagamentos
+                    .Where(p => p.Cartao.Numero == numeroCartao)
+                    .ToList();
+            }
+        }
+    }
+}
